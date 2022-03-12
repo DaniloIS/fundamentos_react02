a5dad79cbf26353d514c67515f59fd2824f44fcf
DaniloIS <danilosantos-pg@hotmail.com>
2022-03-12 00:17:09 -0300

    Componente statico

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -10,14 +10,15 @@ export default function Home() {
       height: '100vh',
       backgroundColor: '#222',
       color: '#eee',
-    }}>'
+    }}>
       <Navigator title='Estilo' href='/style' />
       <Navigator title='Exemplo' href='/exemple' bgcolor='#9400d3' />
       <Navigator title='JSX' href='/jsx' bgcolor='#dc143c' />
       <Navigator title='Navegação #01' href='/navigation' bgcolor='#008000' />
       <Navigator title='Navegação #02' href='/client/sp-1/123' bgcolor='#0000ff' />
-      <Navigator title='Componente com Estato' href='/state' bgcolor='#ffc0cb' />
-      <Navigator title='Integração com API' href='/integration_one' bgcolor='#ffc0cb' />
+      <Navigator title='Componente com Estato' href='/state' bgcolor='#a45b71' />
+      <Navigator title='Integração com API' href='/integration_one' bgcolor='#42a9a9' />
+      <Navigator title='Conteúdo Estático' href='/static' bgcolor='#fa054a' />
     </div>
   ) 
 }
